b2a69c008b4dd3cae00651a51bd32d2cc1e5528a
Abhishek kumar <128677325+Akdevit@users.noreply.github.com>
2024-07-02 10:03:15 +0530

    env change

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_API_URL;
+export const BASE_URL = 'https://streamtube-9voe.onrender.com/';
 
 //http://localhost:5000/
 //https://streamtube-9voe.onrender.com/
